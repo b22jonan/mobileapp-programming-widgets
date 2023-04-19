67b71a446eac56dbec72650090fbd9399f934f18
Jonathan Antfolk <61019188+walsand987@users.noreply.github.com>
2023-04-19 15:11:32 +0200

    removed autoclickers and added listeners

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -11,57 +11,38 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-    int addPerSecond = 0;
-    int TotalCookies = 0;
     // when image button is clicked, take the current value of the counter and increment it by 1 and display the new value
     public void onImageButtonClicked(View view) {
         // get the current value of the counter
-        TextView TextView = (TextView ) findViewById(R.id.textView);
+        TextView TextView = findViewById(R.id.textView);
         String counterString = TextView.getText().toString();
         int counter = Integer.parseInt(counterString);
         // increment the counter
         counter++;
-        TotalCookies++;
+
         // display the new value of the counter
         TextView.setText(Integer.toString(counter));
     }
 
-    // when the noraml button is pressed remove 20 from the counter and add 1 cookie every second
-    public void onNormalButtonClicked(View view) {
-        if (TotalCookies >= 20) {
-            // get the current value of the counter
-            TextView TextView = (TextView ) findViewById(R.id.textView);
-            String counterString = TextView.getText().toString();
-            int counter = Integer.parseInt(counterString);
-            // decrement the counter by 20
-            counter -= 20;
-            TotalCookies -= 20;
-            // display the new value of the counter
-            TextView.setText(Integer.toString(counter));
-            // add 1 cookie every second
-            addPerSecond++;
-        }
+    // when normal button is clicked, add the value in the edit text to the counter and display the new value
+    public void onButtonClicked(View view) {
+        // get the current value of the counter
+        TextView TextView = findViewById(R.id.textView);
+        String counterString = TextView.getText().toString();
+        int counter = Integer.parseInt(counterString);
+
+        // get the value in the edit text
+        TextView EditText = findViewById(R.id.editTextNumber);
+        String editTextString = EditText.getText().toString();
+        int editText = Integer.parseInt(editTextString);
+
+        // add the value in the edit text to the counter
+        counter += editText;
+
+        // display the new value of the counter
+        TextView.setText(Integer.toString(counter));
     }
 
-    // create a thread that adds addPerSecond cookies every second
-    public void run() {
-        while (true) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            // get the current value of the counter
-            TextView TextView = (TextView ) findViewById(R.id.textView);
-            String counterString = TextView.getText().toString();
-            int counter = Integer.parseInt(counterString);
-            // increment the counter
-            counter += addPerSecond;
-            TotalCookies += addPerSecond;
-            // display the new value of the counter
-            TextView.setText(Integer.toString(counter));
-        }
-    }
 
 
     @Override
@@ -69,5 +50,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        ImageButton CookieButton = findViewById(R.id.imageButton);
+
+
+
+        // listeners
+        CookieButton.setOnClickListener(new View.OnClickListener() {
+            // when image button is clicked, take the current value of the counter and increment it by 1 and display the new value
+            @Override
+            public void onClick(View v) {
+                onImageButtonClicked(v);
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener() {
+            // when normal button is clicked, add the value in the edit text to the counter and display the new value
+            @Override
+            public void onClick(View v) {
+                onButtonClicked(v);
+            }
+        });
+
+
+
     }
 }
